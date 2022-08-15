1c9c017e5d5b0c8f1d5efad0563afdad1ad0d980
Ben Voges <108232180+Toblomoro@users.noreply.github.com>
2022-08-15 13:28:03 -0500

    Onto observables..

--- Tour_of_Heroes/src/app/heroes/heroes.component.ts
+++ Tour_of_Heroes/src/app/heroes/heroes.component.ts
@@ -1,30 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {HEROES} from '../mock-heroes';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
-onSelect(hero: Hero): void {
-  this.selectedHero = hero;
-}
-   
-  
-  heroes = HEROES;
-
-  constructor() { 
-   
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-  
-  
 
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+  
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
   ngOnInit(): void {
-    
+    this.getHeroes();
   }
-  
-
 }
 export interface Hero {
   id: number;
